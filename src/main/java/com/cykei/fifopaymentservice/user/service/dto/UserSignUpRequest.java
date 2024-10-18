577d2a508fb3a33ce0977db553668a423e6c433f
cykei <kelle111@naver.com>
2024-10-18 16:48:02 +0900

    로그인 기능 - validation 추가

--- src/main/java/com/cykei/fifopaymentservice/user/service/dto/UserSignUpRequest.java
+++ src/main/java/com/cykei/fifopaymentservice/user/service/dto/UserSignUpRequest.java
@@ -1,16 +1,20 @@
 package com.cykei.fifopaymentservice.user.service.dto;
 
 import com.cykei.fifopaymentservice.user.User;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserSignUpRequest {
+    @Email
     @NotBlank
     private String email;
 
+    @Size(min = 8, max = 12)
     @NotBlank
     private String password;
 
@@ -18,6 +22,7 @@ public class UserSignUpRequest {
     private String name;
 
     private String address;
+
     private String phone;
 
     public User toEntity() {
