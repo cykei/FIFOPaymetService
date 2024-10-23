705e11be208d1efdeaf4d571579605850c33795e
cykei <kelle111@naver.com>
2024-10-23 13:14:39 +0900

    [style] 찜상품 조회 파라미터이름 수정

--- src/main/java/com/cykei/fifopaymentservice/wish/repository/WishRepositoryCustom.java
+++ src/main/java/com/cykei/fifopaymentservice/wish/repository/WishRepositoryCustom.java
@@ -5,5 +5,5 @@ import com.cykei.fifopaymentservice.wish.Wish;
 import java.util.List;
 
 public interface WishRepositoryCustom {
-    List<Wish> getWishes(long productId, int size, Long cursor);
+    List<Wish> getWishes(long userId, int size, Long cursor);
 }
